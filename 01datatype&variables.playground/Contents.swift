a5e6dbe00bf2c02484277dcee9fe3d36f916bedb
Delali Kumapley <delalikumapley@Apples-MacBook-Pro-39.local>
2019-08-06 10:14:44 -0400

    Initial Commit

---
+++ 01datatype&variables.playground/Contents.swift
@@ -0,0 +1,100 @@
+import UIKit
+
+//Examples of strings
+"puppies"
+"1234"
+"Hello, world"
+
+//Example of a variable
+var  address = "10 Hudson Yard"
+address = "48th floor"
+var  companyName : String
+companyName = "Best Company Ever"
+
+//Example of a constant
+let planet = "Earth"
+
+//Example of joining varibles and strings
+var string1 = "Welcome"
+var string2 = "Delali!"
+
+var welcome1 = "\(string1) \(string2)"
+var welcome2 = string1 + " " + string2
+
+print(welcome1)
+print("Here is my sentence!")
+
+//practice
+
+var first = "Karlie"
+var last = "Kloss"
+
+print("\(first)\(last)")
+print(first + last)
+print (first + " " + last)
+print(last + " "+first)
+print(last + " " + first + " " + last + " " + first)
+
+var love = "I love"
+
+print(love + " " + first)
+
+//Examples of integers
+1
+54
+-16
+
+//Examples of floats & doubles
+0.13
+3.14
+2.1
+
+var integer = 4.0
+var double = 5.0
+
+4 * 4
+4 * integer
+integer * integer
+4 * double
+integer * double
+
+//practice
+
+var a = 12
+var b = 65
+var c = 31
+var d = 98
+
+var average = a + b + c + d
+average/4
+
+var average2 = (a + b + c + d)/4
+
+var average3 = (a + b) + (c + d)/4
+
+var average4 = a + (b + c) + d/4
+
+//Practice with datatypes
+
+//name: variable/constant
+//address: variable
+//gradeLevel: variable
+//birthday: constant
+//age: variable
+//eyeColor: constant
+//favoriteColor: variable
+
+var hours = 20.0
+var InAWeek = 10.25 * hours
+
+var celebrity = "Yara Shahidi"
+var basicLine = "Happy Birthday to you"
+var secondLine = "Happy birthday dear"
+
+print("\(basicLine) \(basicLine) \(secondLine) \(celebrity) \(basicLine)")
+
+
+
+
+
+
---
+++ MyPlayground.Conditionals.playground/Contents.swift
@@ -0,0 +1,74 @@
+import UIKit
+
+var str = "Hello, playground"
+//Comparison Operators Practice
+
+5 < 3
+12 > 7
+6 != 8
+"karlie" == "karlie"
+"karlie" == "karliekloss"
+"Karlie" == "karlie"
+
+var luckyNum = 7
+
+luckyNum < 10
+luckyNum == 7
+
+//Examples of Conditionals
+var dogAge = 5
+if dogAge < 2 {
+    print("You are a puppy!🐶")
+}
+else if dogAge > 12 {
+    print("You are elderly!")
+}
+else {
+    print("You are awesome!")
+}
+
+//Practice Conditionals
+var favoriteFood = "Wendys"
+if favoriteFood == "Chipotle" {
+    print ("Queso and Chips too!")
+}
+
+else if favoriteFood == "Starbucks" {
+    print("Their Caramel Ribbon Crunch Frappucinos are the Best!")
+}
+
+else if favoriteFood == "White Castle" {
+    print("That's unfortunate.")
+}
+else if favoriteFood == "Wendys" {
+    print("Good taste!")
+}
+
+else {
+    print("Anything except White Castle Works!")
+    
+}
+
+//Practice Swift Conditionals
+//var hasAccount = "false"
+
+//if hasAccount == "false" {
+  //  print("Let's get some information about you.")
+//}
+
+var gradeLevel = 10
+
+if (gradeLevel < 6) && (gradeLevel >= 1) {
+    print("You are in Elementary School.")
+}
+
+else if (gradeLevel > 5) && (gradeLevel < 9){
+    print("You are in Middle School.")
+}
+
+else if (gradeLevel > 8) && (gradeLevel < 12) {
+    print("You are in High School.")
+}
+
+//W
+
